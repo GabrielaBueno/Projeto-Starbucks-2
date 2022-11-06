326a9fd94d6b81dc522a2b92e54a2ca2027b14c1
sato1001 <81205984+sato1001@users.noreply.github.com>
2022-11-06 20:57:31 -0300

    Update index.js

--- pages/home/index.js
+++ pages/home/index.js
@@ -7,7 +7,16 @@ export default()=>{
         <source srcset="https://s3-sa-east-1.amazonaws.com/manyplaces-p/starbucks/site/banners/banner_195590685_desktop.jpg?crc=7f49f1de" >
         <img alt="Starbucks" src="https://s3-sa-east-1.amazonaws.com/manyplaces-p/starbucks/site/banners/banner_195590685_desktop.jpg?crc=7f49f1de">
     </picture>
-    </div> 
+    </div>
+    <section class="box box-a bg-primary text-center py-md">
+                <div class="box-inner">
+                    <p class="text-md">
+                        Conforme abrimos nossas lojas, permanecemos no compromisso com a saúde e bem-estar de nossos partners e consumidores.
+    
+                    </p>
+                    <button class="btn btn-trans">Saiba mais</button>
+                </div>
+            </section>
     `;
     container.innerHTML=template;
     return container;
