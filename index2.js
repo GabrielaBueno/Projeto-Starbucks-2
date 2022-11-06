35fd5578a438de99754d49fef6dd3d56a0e9c8cf
sato1001 <81205984+sato1001@users.noreply.github.com>
2022-11-06 20:33:58 -0300

    Importando Index

---
+++ index2.js
@@ -0,0 +1,8 @@
+export default()=>{
+    const container = document.createElement('div');
+    const template =`
+    <p>Teste</p>
+    `;
+    container.innerHTML=template;
+    return container;
+}
