239b886bd15633cfe6704a19f66a6b644d7febd9
Gabriela Bueno <68259978+GabrielaBueno@users.noreply.github.com>
2022-11-06 20:44:07 -0300

    Add files via upload

---
+++ pages/dashboard/index.js
@@ -0,0 +1,35 @@
+export default()=>{
+    const container = document.createElement('div');
+    const template =`
+    <link rel="stylesheet" href="teste.css"/>
+    <div class="body-wrapper">
+    
+        <div class="main-container">
+
+            <div class="progress hide r-progress">
+                  <div class="indeterminate"></div>
+              </div>
+
+            <div class="container-fluid">
+                <h3>Seja Bem-vindo!!!</h3>
+                <div class="input-section">
+						<label>Digite a quantidade de raposas a serem exibidas<span class="required-color">*</span></label>
+						<input type="number" placeholder="" id="number" required />
+				</div>
+                <button id="submit-button">Mostrar</button>
+                <ul id="lista">             
+                    <img id="image" src="https://randomfox.ca/images/27.jpg" width="300px" height="300px">
+                </ul>
+                
+            </div>
+        </div>
+    </div>
+    </div>
+    </section>
+    </div>
+
+
+    `;
+    container.innerHTML=template;
+    return container;
+}
