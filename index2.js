978ec2a1d096dca9cf03f38c9abd2b52e663546c
Gabriela Bueno <68259978+GabrielaBueno@users.noreply.github.com>
2022-11-06 20:42:13 -0300

    Delete index2.js

--- index2.js
+++
@@ -1,8 +0,0 @@
-export default()=>{
-    const container = document.createElement('div');
-    const template =`
-    <p>Teste</p>
-    `;
-    container.innerHTML=template;
-    return container;
-}
