754a75109c3ec35a7e57cb4b6192d3dc972e0a38
Gabriela Bueno <68259978+GabrielaBueno@users.noreply.github.com>
2022-12-14 15:00:05 -0300

    Update index1.js

--- pages/dashboard/index1.js
+++ pages/dashboard/index1.js
@@ -14,12 +14,12 @@ export default()=>{
                 <h3>Seja Bem-vindo!!!</h3>
                 <div class="input-section">
                     <h3>Raposas!!!</h3>
+		    <div id="divBusca">
+  			<img src="" alt="Buscar..."/>
+  			<input type="text" id="txtBusca" placeholder="Buscar..."/>
+ 			 <button id="btnBusca">Buscar</button>
+		    </div>
 				</div>
-	 <div id="divBusca">
-  		<img src="" alt="Buscar..."/>
- 		 <input type="text" id="txtBusca" placeholder="Buscar..."/>
- 		 <button id="btnBusca">Buscar</button>
-	</div>
                 <ul id="lista">
                 </ul>
                 
