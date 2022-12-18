d6ef5558610abb89479953c9dfcbce52e5264a98
Gabriela Bueno <68259978+GabrielaBueno@users.noreply.github.com>
2022-12-17 23:17:43 -0300

    Update index1.js

--- pages/dashboard/index1.js
+++ pages/dashboard/index1.js
@@ -1,32 +1,15 @@
 export default()=>{
     const container = document.createElement('div');
-    const template =`
-    <link rel="stylesheet" href="teste.css"/>
-    <div class="body-wrapper">
+    const template =
+    <body>
+        <h1>Digite seu CEP
+        </h1>
+        <input type='text' id='cep' name='cep' size='15' maxlenght='8' autocomplete='off'>
+        <button id='btn'>Consultar</button>
+        <div style='padding-top:20px;' id='return'></div>
+    </body>
     
-        <div class="main-container">
-
-            <div class="progress hide r-progress">
-                  <div class="indeterminate"></div>
-              </div>
-
-            <div class="container-fluid">
-                <h3>Seja Bem-vindo!!!</h3>
-                <div class="input-section">
-                    <h3>Raposas!!!</h3>
-	    </div>
-                <ul id="lista">
-                </ul>
-                
-            </div>
-        </div>
-    </div>
-    </div>
-    </section>
-    </div>
-
-
-    `;
+    ;
     container.innerHTML=template;
     return container;
 }
