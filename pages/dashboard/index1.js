484307cd0349aeae5ee93d9d37a86dbc6c556199
Gabriela Bueno <68259978+GabrielaBueno@users.noreply.github.com>
2022-12-18 16:17:42 -0300

    Update index1.js

--- pages/dashboard/index1.js
+++ pages/dashboard/index1.js
@@ -13,7 +13,7 @@ export default()=>{
      <body>
         <h3> Seja Bem-vindo!</h3>
         <h4>Informe o nome da imagem: </h4>
-        <input type='text' id='img' name='img' size='15' maxlenght='8' autocomplete='off'>
+       <input type='text' id='cep' name='cep' size='15' maxlenght='8' autocomplete='off'>
         <button id='btn'>Consultar</button>
         <div style='padding-top:20px;' id='return'></div>
     </body>
