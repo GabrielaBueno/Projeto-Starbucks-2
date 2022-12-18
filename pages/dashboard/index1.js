cd193a5194bf266663dd0409a362bedc8d03bb6c
Gabriela Bueno <68259978+GabrielaBueno@users.noreply.github.com>
2022-12-18 18:01:45 -0300

    Update index1.js

--- pages/dashboard/index1.js
+++ pages/dashboard/index1.js
@@ -2,18 +2,18 @@ export default()=>{
     const container = document.createElement('div');
     const template =`
     <link rel="stylesheet" href="teste.css"/>
-    <div class="body-wrapper">
+  /* <div class="body-wrapper">
     
         <div class="main-container">
 
             <div class="progress hide r-progress">
                   <div class="indeterminate"></div>
-              </div>
+              </div> */
 
      <body>
         <h3> Seja Bem-vindo!</h3>
         <h4>Informe o nome da imagem: </h4>
-       <input type='text' id='img' name='img' size='15' maxlenght='8' autocomplete='off'>
+       <input type='text' id='cep' name='cep' size='15' maxlenght='8' autocomplete='off'>
         <button id='btn'>Consultar</button>
         <div style='padding-top:20px;' id='return'></div>
     </body>
@@ -22,8 +22,8 @@ export default()=>{
     
     </div>
     </div>
- //   </section>
-  //  </div>
+ //  </section>
+ //  </div>
 
 
     `;
