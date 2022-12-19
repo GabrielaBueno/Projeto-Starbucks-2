61b8f895cce338a74f34a07fe336227160b0ea18
Gabriela Bueno <68259978+GabrielaBueno@users.noreply.github.com>
2022-12-18 23:10:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import dashboard1 from "./pages/dashboard/index1.js";
 const btn=document.getElementById("btn-entrar");
 const btn1=document.getElementById("btn-participe");
 const main=document.querySelector('#root');
+const lista=document.getElementById("lista");
+
 
 
 function logado(){
@@ -359,18 +361,9 @@ const init=()=>{
           break;
       case "#dashboard1":
         
+        btn.style.display = 'none';
+        btn1.style.display = 'none';
         if(logado()){
-          btn.style.display = 'none';
-          btn1.style.display = 'none';
-          main.appendChild(dashboard1());
-          const btn = document.getElementById('btn');
-          
-          console.log(btn);
-          btn.addEventListener("click", (e) => {
-            console.log(btn);
-            e.preventDefault();
-            
-            if(logado()){
           
           main.appendChild(dashboard1());
           const btn = document.getElementById('btn');
@@ -382,26 +375,39 @@ const init=()=>{
             
             consultarImg();
             function consultarImg(){
-              var img = document.getElementById("cep").value;
-              var url='http://localhost:3000/files/'+img;
+              var img = document.getElementById("img").value;
+              var url='http://ec2-3-88-184-58.compute-1.amazonaws.com:3000/files/'+img;
               var request=new XMLHttpRequest();
               request.open('GET', url, true);
               request.onerror=function(e){
                 document.getElementById('return').innerHTML='API Offline ou nome invalido'
               }
               request.onload=()=>{
-                var response=JSON.parse(request.responseText);
+                var response='json';
                 if(response.erro==true){
                   document.getElementById('return').innerHTML='nome não localizado'
                 }else
                   document.getElementById('return').innerHTML='img:'+response.name;
                 }
+                request.send();
+                let newimage=document.createElement('img');
+                newimage.src = img.image;
+                lista.appendChild(newimage);
+                
               }
               
-              request.send();
+             //request.send();
             })
           
         }
+        else{
+          window.location.hash="login";
+        }
+      default:
+        //main.appendChild(home());
+    }
+  })
+}
 window.addEventListener("load", ()=>{
   main.appendChild(home());
   init();
