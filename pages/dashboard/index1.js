3ce083ce971562a87caecad47fa2d4aeda0091f9
Gabriela Bueno <68259978+GabrielaBueno@users.noreply.github.com>
2022-12-18 23:56:20 -0300

    Update index1.js

--- pages/dashboard/index1.js
+++ pages/dashboard/index1.js
@@ -1,6 +1,6 @@
 export default()=>{
     const container = document.createElement('div');
-    const template =
+    const template = `
     <body>
         <h4>Seja Bem-vindo! </h4>
         <h1>Digite o nome da Imagem:
@@ -9,8 +9,7 @@ export default()=>{
         <button id='btn'>Consultar</button>
         <div style='padding-top:20px;' id='return'></div>
     </body>
-    <span id="element"></span>
-    ;
+    <span id="element"></span>;
     
     container.innerHTML=template;
     return container;
