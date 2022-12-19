4d3fdf5a2225d484377f9d49ee457f4cdda8b69b
Gabriela Bueno <68259978+GabrielaBueno@users.noreply.github.com>
2022-12-18 23:11:50 -0300

    Update index1.js

--- pages/dashboard/index1.js
+++ pages/dashboard/index1.js
@@ -2,13 +2,14 @@ export default()=>{
     const container = document.createElement('div');
     const template =`
     <body>
-        <h1>Digite seu cep
+        <h4>Seja Bem-vindo! </h4>
+        <h1>Digite o nome da Imagem:
         </h1>
-        <input type='text' id='cep' name='cep' size='15' maxlenght='8' autocomplete='off'>
-        <button id='btn'>Concultar</button>
+        <input type='text' id='img' name='img' size='15' maxlenght='8' autocomplete='off'>
+        <button id='btn'>Consultar</button>
         <div style='padding-top:20px;' id='return'></div>
     </body>
-    
+    <ul id="lista"> <img id="image"  width="300px" height="300px"> </ul>
     `;
     container.innerHTML=template;
     return container;
